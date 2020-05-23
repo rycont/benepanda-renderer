3160d8049d42a56caf189ec6bb0a374f24c9d868
rycont <reactdev@kakao.com>
2020-05-23 22:04:34 +0900

    runnable

---
+++ renderer.ts
@@ -0,0 +1,46 @@
+import { Paper } from "./type";
+import pdf from "html-pdf";
+import { exec } from "child_process";
+import { writeFileSync, unlink, readFileSync } from 'fs'
+import { promisify } from 'util'
+
+const pexec = promisify(exec)
+const pcreate = promisify(pdf.create)
+const punlink = promisify(unlink)
+
+function makeRandomString(length: number): string {
+    return Array(length).fill(0).map(e => String.fromCharCode((Math.random() * 25 + 65))).join('')
+}
+
+const prefix = readFileSync('./prefix.html', {
+    encoding: "utf-8"
+})
+
+export async function renderPDF(paper: Paper): Promise<string> {
+    console.time('start')
+
+    const htmlString = prefix + paper.Table01.map((e) => `
+        ${e.EXE_RANGE ? `<p class="range">${e.EXE_RANGE}</p>` : ''}
+
+         ${e.EXE_HTML ? `<p>${e.EXE_HTML}</p>` : ''}
+
+                         <p class="number">${e.IBQ_NUM}.</p>
+                          
+         ${e.QST_HTML ? `<p class="number">${e.QST_HTML}</p>` : ''}
+        `).join('').split(/width=".*?"/).join('').split('&nbsp;&nbsp;&nbsp;').join(' ')
+
+    writeFileSync('./index.html', htmlString)
+    const filename = makeRandomString(20);
+
+    await (() => new Promise((res) => pdf
+        .create(htmlString, {
+            width: "10.5cm",
+            height: "29.7cm",
+            base: "http://q.benedu.co.kr",
+        }).toFile(`./TEMP_${filename}.pdf`, res)))()
+
+    const { stderr: jamErr, stdout: output } = await pexec(`pdfjam --nup 2x1 --scale 0.95 ./TEMP_${filename}.pdf --outfile ./pdf/${filename}.pdf`)
+    console.log(output)
+    await punlink(`./TEMP_${filename}.pdf`)
+    return `./pdf/${filename}.pdf`
+}
--- server.ts
+++ server.ts
@@ -3,15 +3,26 @@ import Router from 'koa-router'
 import send from 'koa-send'
 import bodyParser from 'koa-bodyparser'
 
-import { renderPDF } from './'
+import { renderPDF } from './renderer'
+import { existsSync } from 'fs'
 
 const app = new Koa()
 const router = new Router()
 app.use(bodyParser());
 
-router.post('/', async (ctx, next) => {
-    const filepath = await renderPDF(ctx.request.body)
-    await send(ctx, filepath)
+router.post('/renderPdf', async (ctx, next) => {
+    console.log('ㅁ.. 뭔가 들어옴..', ctx.request.body)
+    const filename = await renderPDF(ctx.request.body)
+    ctx.body = {
+        filename
+    }
+})
+
+router.get('/pdf/:url', async (ctx) => {
+    const { url } = ctx.params
+    console.log(url, url.length, url.length === 24, url.slice(-4) === '.pdf')
+    if (url.length === 24 && url.slice(-4) === '.pdf' && existsSync(`./pdf/${ctx.params.url}`)) await send(ctx, `./pdf/${ctx.params.url}`);
+    else ctx.body = '지랄마'
 })
 
 app
