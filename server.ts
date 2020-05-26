436953a7a55d97dec4cd0b1eec8a0357692d8f28
rycont <reactdev@kakao.com>
2020-05-26 21:06:53 +0900

    move upload step to server

--- server.ts
+++ server.ts
@@ -3,6 +3,9 @@ import Router from 'koa-router'
 import bodyParser from 'koa-bodyparser'
 
 import { renderPDF } from './renderer'
+import admin from 'firebase-admin';
+
+admin.initializeApp()
 
 const app = new Koa()
 const router = new Router()
@@ -12,6 +15,11 @@ const port = process.env.port || 8080
 
 router.post('/renderPdf', async (ctx) => {
     const filename = await renderPDF(ctx.request.body)
+    const bucket = admin.storage().bucket('benepanda-renderer.appspot.com');
+    await bucket.upload(`./pdf/${filename}.pdf`, {
+        destination: `${filename}.pdf`
+    })
+    await bucket.upload(`./thumbnail/${filename}_thumbnail.png`)
     ctx.body = {
         pdf: `${filename}.pdf`,
         thumbnail: `${filename}_thumbnail.png`
